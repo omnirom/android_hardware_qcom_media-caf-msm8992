e80a01da7311a98f4286d54c99464e3c70aa76c5
Jake Whatley <jacob.whatley91@gmail.com>
2016-05-31 02:06:50 +0200

    media-caf-msm8992: don't let the man hold you down
    Change-Id: Ia181783645bc41b79ab0d8b37e35131b0d5827f4

--- Android.mk
+++ Android.mk
@@ -1,20 +1,10 @@
 ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf-msm8992)
 QCOM_MEDIA_ROOT := $(call my-dir)
-$(warning target list is : $(MSM_VIDC_TARGET_LIST))
 
-#Compile these for all targets under QCOM_BOARD_PLATFORMS list.
-ifneq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
-endif
 
-ifneq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true)
 include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
 include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 
-ifeq ($(TARGET_BOARD_PLATFORM),apq8084)
-include $(QCOM_MEDIA_ROOT)/videopp/Android.mk
-endif
-
-endif
 endif
