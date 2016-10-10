cfd417f2ee191ecdca30eea7cd0f8c6e772001e8
Jake Whatley <jacob.whatley91@gmail.com>
2016-10-10 07:15:27 +0200

    vdec: allow unused parameters

--- mm-video-v4l2/vidc/vdec/Android.mk
+++ mm-video-v4l2/vidc/vdec/Android.mk
@@ -100,6 +100,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE                    := libOmxVdec
 LOCAL_MODULE_TAGS               := optional
 LOCAL_CFLAGS                    := $(libmm-vdec-def) -Werror
+LOCAL_CFLAGS                    := $(libmm-vdec-def) -Wunused-parameter
 LOCAL_CLANG := false
 LOCAL_C_INCLUDES                += $(libmm-vdec-inc)
 LOCAL_ADDITIONAL_DEPENDENCIES   := $(libmm-vdec-add-dep)
